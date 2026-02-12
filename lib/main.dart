a15543cc01ac3fb47ed9c33d6afeeafe2c0f6eb0
Jonas N. <jn.jonasneumann@googlemail.com>
2026-02-12 12:36:50 +0100

    fix indent

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,30 @@ import 'package:flutter/widgets.dart';
 import 'package:prepping_penguin/widgets/nav_bar.dart';
 
 void main() {
-  runApp(const MyApp());
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+    const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Color(0xFFEFEFEF)),
-      child: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              'Hello World',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(fontSize: 32, color: Color(0x00000000)),
+    // This widget is the root of your application.
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            decoration: const BoxDecoration(color: Color(0xFFEFEFEF)),
+            child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    Center(
+                    child: Text(
+                        'Hello World',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(fontSize: 32, color: Color(0x00000000)),
+                    ),
+                    ),
+                        NavBar(),
+                ],
             ),
-          ),
-          NavBar(),
-        ],
-      ),
-    );
-  }
+        );
+    }
 }
--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/widgets.dart';
 
 class NavBar extends StatelessWidget {
-  const NavBar({super.key});
+    const NavBar({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Color.fromARGB(255, 25, 109, 91)),
-      child: const Center(child: Text("bar", textDirection: TextDirection.ltr)),
-    );
-  }
-}
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            decoration: const BoxDecoration(color: Color.fromARGB(255, 25, 109, 91)),
+            child: const Center(child: Text("bar", textDirection: TextDirection.ltr)),
+        );
+    }
+}s
